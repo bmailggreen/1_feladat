634b3f4d4ad5d683b04846a2b13094f2fa54b9ef
bmailggreen <bmailggreen@gmail.com>
2022-06-17 20:30:51 +0200

    Version 1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void hello() {
-        System.out.println("Helló Világ!");
+        System.out.println("Helló Világ és Én!");
     }
 
     public static void main(String[] args) {
