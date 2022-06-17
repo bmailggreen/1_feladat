9d45130d5564714507c23a3019038abb2f85db3e
bmailggreen <bmailggreen@gmail.com>
2022-06-17 20:24:32 +0200

    Version 1.0

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void hello() {
+        System.out.println("Helló Világ!");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+}
